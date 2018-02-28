95a2aa9d1011a9c7dd82a206cd8d2cb1e8c1fc59
Srujana <srujana.vemula@wohlig.com>
2018-02-28 14:06:14 +0530

    done

--- frontend/js/factories/navigation.js
+++ frontend/js/factories/navigation.js
@@ -1,25 +1,26 @@
 myApp.factory('NavigationService', function () {
     var navigation = [{
-            name: "Home",
-            classis: "active",
-            anchor: "home",
-            subnav: [{
-                name: "Subnav1",
-                classis: "active",
-                anchor: "home"
-            }]
-        }, {
-            name: "Form",
-            classis: "active",
-            anchor: "form",
-            subnav: []
-        },
-        {
-            name: "Grid",
-            classis: "active",
-            anchor: "grid",
-            subnav: []
-        }
+            // name: "Account Details",
+            // classis: "active",
+            // anchor: "home",
+            // subnav: [{
+            //     name: "Subnav1",
+            //     classis: "active",
+            //     anchor: "home"
+            // }]
+        }, 
+        // {
+        //     name: "Form",
+        //     classis: "active",
+        //     anchor: "form",
+        //     subnav: []
+        // },
+        // {
+        //     name: "Grid",
+        //     classis: "active",
+        //     anchor: "grid",
+        //     subnav: []
+        // }
     ];
 
     return {
